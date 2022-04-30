cde24f70716ca98dc8ad6883c310916134751aa1
issaRonin <101780577+issaRonin@users.noreply.github.com>
2022-04-29 20:41:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function Dementia() {
+    document.getElementById("imagerick").src="https://i.postimg.cc/6qygsjVp/c4b.png";
+    document.getElementById("dementia").play();
+    document.getElementById("title").innerHTML="Wait...what happened rick?? whos rick??? what?";
+    document.getElementById("rickmusic").pause();
+}
+
+function Rick() {
+document.getElementById("imagerick").src="https://i.postimg.cc/pd7j1Ms4/thericck.jpg";
+document.getElementById("rickmusic").play();
+document.getElementById("title").innerHTML="No way its rick astley guys!!";
+document.getElementById("dementia").pause();
+}
